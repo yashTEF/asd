6ccedb0914be8c9092646dc427dca7fe7061de31
ShashankPakhare <shashankpakhare25@gmail.com>
2020-11-28 15:26:07 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var empMode= require('../modules/employee');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
 
 module.exports = router;
+var employee=empMode.find({});
+
+router.get('/', function(req, res, next) {
+  employee.exec(function(err,data){
+    if(err) throw err;
+    res.render('index',{title:'Employee Records', records:data});
+  });
+});
