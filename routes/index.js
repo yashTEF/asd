7892247492894298915b70cb00e99023830a8dc8
ShashankPakhare <shashankpakhare25@gmail.com>
2020-11-28 16:24:25 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,35 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/',function(req, res, next) {
+
+  var empdetails=new empMode({
+    name:req.body.name,
+    email:req.body.mail,
+    etype:req.body.etype,
+    hourlyrate:req.body.hrate,
+    totalhour:req.body.totalhour,
+
+  });
+
+
+  empdetails.save(function(err,res1){
+    if(err) throw err;
+
+    employee.exec(function(err,data){
+      if(err) throw err;
+      res.render('index',{title:'Employee Records', records:data});
+    });
+
+  });
+  
+});
+
+router.get('/login',function(err,res,next){
+  res.render('login');
+})
+
+
 router.get('/signup',function(err,res,next){
   res.render('signup',{msg:''});
 })
