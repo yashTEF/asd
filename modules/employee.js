a48fde3ff658375a6a115cce17d55a0067fd8149
ShashankPakhare <shashankpakhare25@gmail.com>
2020-11-28 15:33:58 +0530

    make it better

--- modules/employee.js
+++ modules/employee.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://new-Shashank:ShAsHaNk@cluster0.loytd.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true});
 var conn=mongoose.connection;
+
 //var conn =mongoose.Collection;
+
 var employeeSchema=new mongoose.Schema({
     name: String,
     email: String,
