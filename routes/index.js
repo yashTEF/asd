2b11bc8783704697fc615fd42066c7a8593dd206
ShashankPakhare <shashankpakhare25@gmail.com>
2020-12-02 02:04:58 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -230,3 +230,5 @@ router.get('/font',checkLoginUser,function(err,res,next){
 router.get('/learn',function(err,res,next){
   res.render('learn');
 })
+//client id
+//80181116872-qnom0akloqd0muvo1n46vtm5gb9st820.apps.googleusercontent.com
